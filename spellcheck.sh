23654279003a5eb2836c257cac92f1b920217a62
Akuli <akuviljanen17@gmail.com>
2021-05-24 18:34:38 +0300

    include line numbers in spellcheck.sh output

--- spellcheck.sh
+++ spellcheck.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 set -e
 
-rm -f misspelled.txt
-
 # Sometimes commit hashes contain a few consecutive letters, aspell thinks those are words
 function delete_commit_hashes()
 {
     sed 's/\b[0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f]\b//g'
 }
 
-for file in *.md; do
-    cat $file \
-        | delete_commit_hashes \
-        | aspell -l en list \
-        | (grep -v -f spellcheck_exclude.txt || true) \
-        | sed "s/^/$file: /g" \
-        >> misspelled.txt
-done
-
-cat misspelled.txt | sort | uniq
-! [ -s misspelled.txt ]
+! grep -n -o -f <(
+    # This subcommand outputs misspelled words in all files, one per line
+    cat *.md | delete_commit_hashes | aspell -l en list | (grep -v -f spellcheck_exclude.txt || true)
+) *.md
