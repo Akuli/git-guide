1de62b28ef8c0f127c2c10b9ab221acd8263dec8
Akuli <akuviljanen17@gmail.com>
2021-05-24 19:51:48 +0300

    spellcheck fixes

--- spellcheck.sh
+++ spellcheck.sh
@@ -7,7 +7,7 @@ function delete_commit_hashes()
     sed 's/\b[0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f]\b//g'
 }
 
-! grep -n -o -f <(
+! grep -nowf <(
     # This subcommand outputs misspelled words in all files, one per line
-    cat *.md | delete_commit_hashes | aspell -l en list | (grep -v -f spellcheck_exclude.txt || true)
+    cat *.md | delete_commit_hashes | aspell -l en list | (grep -vxf spellcheck_exclude.txt || true)
 ) *.md
