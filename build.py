fb32572344a1eb2dc89b5958221f354c270838c0
Akuli <akuviljanen17@gmail.com>
2021-05-28 00:34:45 +0300

    debug print

--- build.py
+++ build.py
@@ -110,6 +110,7 @@ def create_runner():
         git add .
         git commit -q -m "Initial commit"
         git config receive.denyCurrentBranch ignore
+        git config --list
         ''',
         shell=True,
         check=True,
