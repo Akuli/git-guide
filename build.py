0286e6cdfd1e80d384aca8c5d63a7fa0f243d93c
Akuli <akuviljanen17@gmail.com>
2021-05-28 01:00:38 +0300

    fix colors (#28)
    * debug print
    
    * color.ui true
    
    * config the cloned thing
    
    * color???

--- build.py
+++ build.py
@@ -27,6 +27,7 @@ class CommandRunner:
         self.git_config = {
             'core.pager': 'cat',
             'core.editor': 'true',  # Don't change commit message (for merge commits)
+            'color.ui': 'always',
             # Ensure we get same error as with freshly installed git
             'user.email': '',
             'user.name': '',
@@ -106,10 +107,10 @@ def create_runner():
         git init -q
         git config user.email "you@example.com"
         git config user.name "yourusername"
+        git config receive.denyCurrentBranch ignore
         git checkout -q -b main
         git add .
         git commit -q -m "Initial commit"
-        git config receive.denyCurrentBranch ignore
         ''',
         shell=True,
         check=True,
